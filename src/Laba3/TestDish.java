fb1006e952e65daff0120c4131aa066b9d56468b
sMike1 <mihail@svalov.ru>
2022-12-01 21:07:03 +0300

    .

--- src/Laba3/TestDish.java
+++ src/Laba3/TestDish.java
@@ -5,6 +5,7 @@ public class TestDish {
         Plate p1 = new Plate("glass", "white", "big");
         Cup c1 = new Cup("glass", "transparent", 0.5);
         System.out.println(c1.getColor());
+        System.out.println(p1.getColor());
         System.out.println(p1);
         System.out.println(c1);
     }
--- src/Laba7/ArrayListTest.java
+++ src/Laba7/ArrayListTest.java
@@ -2,23 +2,27 @@ package Laba7;
 import java.util.ArrayList;
 
 public class ArrayListTest{
-    public static void maim(String[] args) {
+    public static void main(String[] args) {
         ArrayList<String> states = new ArrayList<String>();
         states.add("Турция");
         states.add("Россия");
-        states.add("мексика");
+        states.add("Мексика");
         states.add("Финляндия");
         states.add("Испания");
         states.add(1, "Франция");
         for (String state : states) {
             System.out.print(state + " ");
         }
+        System.out.println();
         System.out.println(states.get(1));
         states.set(1, "Белоруссия");
-        states.remove("мексика");
-        if(states.contains("Франция")){
-            System.out.println("список содержит государство Франция");
+        states.remove("Мексика");
+        for (String state : states) {
+            System.out.print(state + " ");
+            System.out.println();
         }
-
+        if(states.contains("Белоруссия")){
+            System.out.println("список содержит государство Белоруссия");
+        }
     }
 }
